bc2d1f974833293bc9a2b73d79ca19427af3adf2
TheoParamelle <theo.paramelle@epitech.eu>
2022-10-17 09:40:25 +0200

    push online

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - New Age v6.0.6 (https://startbootstrap.com/theme/new-age)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+* Start Bootstrap - Angel the guardian v6.0.6 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2022 Théo Paramelle
+* Licensed under MIT (https://github.com/StartBootstrap/Angel/blob/master/LICENSE)
 */
 //
 // Scripts
